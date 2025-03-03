79afe0f5bd5f8c2442700782c61ffffd8165ca36
Sinigaglia Lorenzo <lorenzo.sinigaglia@ynov.com>
2025-03-03 17:19:05 +0100

    exercice(easy): Array

---
+++ src/fr/ynov/java/easy/Array.java
@@ -0,0 +1,10 @@
+package fr.ynov.java.easy;
+
+public class Array {
+    public static void main(String[] args) {
+        String[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        for (int i = 0; i < alphabet.length; i++) {
+            System.out.println(alphabet[i]);
+        }
+    }
+}
