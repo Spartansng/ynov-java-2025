bd74410da8bce9f33efd2caefafdf251de15a729
Sinigaglia Lorenzo <lorenzo.sinigaglia@ynov.com>
2025-03-03 17:06:02 +0100

    exercice(easy): hello world/for/while

--- src/fr/ynov/java/easy/HelloWorld.java
+++ src/fr/ynov/java/easy/HelloWorld.java
@@ -3,6 +3,8 @@ package fr.ynov.java.easy;
 public class HelloWorld {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World !");
+
     }
 }
---
+++ src/fr/ynov/java/easy/While.java
@@ -0,0 +1,12 @@
+package fr.ynov.java.easy;
+
+public class While {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 10) {
+            a++;
+        System.out.println(a);
+        }
+        }
+    }
+
